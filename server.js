cfca422c5d4fd9f44c1b003afdda3a9733b9df33
Alika Choo <alikachoo5@gmail.com>
2021-07-09 17:04:39 +0800

    Create api routes

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Define Routes, to actually get to the routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server started on port ${PORT}'));

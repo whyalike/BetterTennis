578759714177421b69215662af2504eb6622d556
Alika Choo <alikachoo5@gmail.com>
2021-07-11 16:23:22 +0800

    Create custom auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route   Get api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res)=> res.send('Auth route'))
+router.get('/', auth, async (req, res)=> {
+    // Using auth as our middleware makes our route protected
+    try {
+        // -password avoids us from returning the password 
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }  catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}); // auth makes route protected
 
 module.exports = router;

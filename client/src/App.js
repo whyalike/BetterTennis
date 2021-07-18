0a37276a52b489feb3bf2eba9c8c3b60a3251834
Alika Choo <alikachoo5@gmail.com>
2021-07-18 18:59:01 +0800

    Update alert component and action call

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Alert from './components/layouts/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
@@ -16,6 +17,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />

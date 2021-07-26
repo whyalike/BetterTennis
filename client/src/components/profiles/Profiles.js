7e55a161cd2e65f60755594aa9b684835291febd
Alika Choo <alikachoo5@gmail.com>
2021-07-26 21:35:42 +0800

    Add space between icons

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -18,7 +18,7 @@ const Profiles = ({getProfiles, profile: {profiles, loading}}) => {
         <Fragment>
           <h1 className='large text-primary'>Tennis Athletes</h1>
           <p className='lead'>
-            <i className='fab fa-connectdevelop'></i>Browse and connect with
+            <i className='fab fa-connectdevelop'></i> Browse and connect with
             other athletes
           </p>
           <div className='profiles'>

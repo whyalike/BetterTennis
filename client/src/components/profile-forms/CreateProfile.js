542769a20e509a3a946f1db397d03aa70dd25007
Alika Choo <alikachoo5@gmail.com>
2021-08-21 23:48:23 +0800

    Add submit button for default createprofile

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -182,6 +182,7 @@ const CreateProfile = ({createProfile, history}) => {
             <input type='submit' className='btn btn-primary my-1' />
           </Fragment>
         )}
+        <input type='submit' className='btn btn-primary my-1' />
         <Link className='btn btn-light my-1' to='/dashboard'>
           Go Back
         </Link>
